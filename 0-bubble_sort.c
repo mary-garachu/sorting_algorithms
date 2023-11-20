df5dbd0d86ab8d77a56cb61de4bdabec147a7cca
sonie <garachu.muthoni@gmail.com>
2023-11-19 23:43:07 -0800

    more corrections to 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,19 +30,3 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
-
-/**
- * swap - Function that swaps two values
- *
- * @a: First value
- * @b: Second value
- * Return: 0
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *b;
-	*b = *a;
-	*a = tmp;
-}
-*/
