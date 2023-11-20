48c5363cfa02302f8a9c04c2ae44e67c1e09fdb5
sonie <garachu.muthoni@gmail.com>
2023-11-19 23:40:24 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,18 +10,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	int tmp;
+	size_t i, j;
 
 	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
@@ -34,7 +37,6 @@ void bubble_sort(int *array, size_t size)
  * @a: First value
  * @b: Second value
  * Return: 0
- */
 void swap(int *a, int *b)
 {
 	int temp;
@@ -43,3 +45,4 @@ void swap(int *a, int *b)
 	*b = *a;
 	*a = tmp;
 }
+*/
